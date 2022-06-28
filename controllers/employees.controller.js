dafcb2972f5ce9e339cdcbd71b7f28d1ec2ea9d4
Sanat Gawade <sanat.leo9@gmail.com>
2022-06-28 19:54:16 +0530

    changes in employees controller

---
+++ controllers/employees.controller.js
@@ -0,0 +1,14 @@
+const jwt = require("jwt");
+const bcrypt = require("bcrypt");
+
+exports.createEmployee = async (res, req) => {
+    
+}
+
+exports.updateEmployee = async (res, req) => {
+
+}
+
+exports.deleteEmployee = async (res, req) => {
+
+}
